7652f2b10679a4a8af40528143ec9dce30b2cf01
AliKhokhar123 <133815650+AliKhokhar123@users.noreply.github.com>
2024-05-31 17:48:48 +0500

    first change

---
+++ public/script.js
@@ -0,0 +1,345 @@
+var loanChartInstance;
+var loanPaymentChartInstance;
+
+function initSlider(sliderId, valueDisplayId, start, min, max, step) {
+    var slider = document.getElementById(sliderId);
+    var valueDisplay = document.getElementById(valueDisplayId);
+
+    noUiSlider.create(slider, {
+        start: start,
+        step: step,
+        connect: [true, false],
+        range: {
+            'min': min,
+            'max': max
+        }
+    });
+
+    slider.noUiSlider.on('update', function (values) {
+        valueDisplay.innerHTML = parseFloat(values[0]).toFixed(2);
+    });
+
+    slider.noUiSlider.on('change', calculate);
+}
+
+function initSliders() {
+    initSlider('monthlyRevenueSlider', 'monthlyRevenueValue', 15000, 0, 50000, 1000);
+    initSlider('monthlyExpensesSlider', 'monthlyExpensesValue', 7000, 0, 50000, 1000);
+    initSlider('investmentAmountSlider', 'investmentAmountValue', 20000, 0, 100000, 1000);
+    initSlider('loanAmountSlider', 'loanAmountValue', 150000, 0, 500000, 5000);
+    initSlider('interestRateSlider', 'interestRateValue', 7.5, 0, 20, 0.1);
+    initSlider('loanTermSlider', 'loanTermValue', 10, 1, 20, 1);
+
+    calculate();
+}
+
+document.addEventListener('DOMContentLoaded', initSliders);
+
+function calculate() {
+    var monthlyRevenue = parseFloat(document.getElementById('monthlyRevenueSlider').noUiSlider.get());
+    var monthlyExpenses = parseFloat(document.getElementById('monthlyExpensesSlider').noUiSlider.get());
+    var investmentAmount = parseFloat(document.getElementById('investmentAmountSlider').noUiSlider.get());
+    var loanAmount = parseFloat(document.getElementById('loanAmountSlider').noUiSlider.get());
+    var interestRate = parseFloat(document.getElementById('interestRateSlider').noUiSlider.get()) / 100;
+    var loanTerm = parseFloat(document.getElementById('loanTermSlider').noUiSlider.get()) * 12;
+
+    var monthlyOperationIncome = monthlyRevenue - monthlyExpenses;
+    var annualOperationIncome = monthlyOperationIncome * 12;
+    var monthlyLoanPayment = calculateLoanPayment(loanAmount, interestRate, loanTerm);
+    var totalPayments = monthlyLoanPayment * loanTerm;
+    var totalInterestPaid = totalPayments - loanAmount;
+    var adjustedMonthlyProfit = monthlyOperationIncome - monthlyLoanPayment;
+    var adjustedAnnualProfit = adjustedMonthlyProfit * 12;
+
+    var totalBusinessPrice = investmentAmount + loanAmount;
+    var businessInterestRate = (annualOperationIncome / totalBusinessPrice) * 100;
+    var annualLoanPayments = monthlyLoanPayment * 12;
+    var personalInvestmentInterest = ((annualOperationIncome - annualLoanPayments) / investmentAmount) * 100;
+
+    var yearsToPayOffInterest = (annualOperationIncome > 0) ? totalInterestPaid / annualOperationIncome : 'N/A';
+    var yearsToPayOffLoan = (annualOperationIncome > 0) ? totalPayments / annualOperationIncome : 'N/A';
+    
+
+    var resultHTML = `
+    <p>Monthly Operational Income: <b>${formatNumber(monthlyOperationIncome)}</b></p>
+    <p>Annual Operational Income: <b>${formatNumber(annualOperationIncome)}</b></p>
+    <p>Total Business Valuation: <b>${formatNumber(totalBusinessPrice)}</b></p>
+    <p>Return on Business Purchase Price: <b>${businessInterestRate.toFixed(2)}%</b></p>
+    <p>Return on Personal Investment: <b>${personalInvestmentInterest.toFixed(2)}%</b></p>
+    <p>Time to Recoup Loan Interest with Income:<b> ${yearsToPayOffInterest.toFixed(2)} Years </b></p>
+    <p>Time Till you can own 100% of the business: <b>${yearsToPayOffLoan.toFixed(2)} Years </b></p>
+    <p>Cumulative Loan Payments: <b>${formatNumber(totalPayments)}</b></p>
+    <p>Average Monthly Loan Payment: <b>${formatNumber(monthlyLoanPayment)}</b></p>
+    <p>Total Interest Incurred on Loan: <b>${formatNumber(totalInterestPaid)}</b></p>
+    <p>Net Monthly Profit (Post-Loan Payments): <b>${formatNumber(adjustedMonthlyProfit)}</b></p>
+    <p>Net Annual Profit (Post-Loan Payments): <b>${formatNumber(adjustedAnnualProfit)}</b></p>
+    `;
+
+    document.getElementById('result').innerHTML = resultHTML;
+    document.getElementById('result').style.display = 'block';
+
+    drawPieChart(loanAmount, totalInterestPaid);
+    drawLoanPaymentChart(adjustedMonthlyProfit, monthlyLoanPayment);
+}
+
+
+function clearForm() {
+    initSliders();
+    document.getElementById('result').style.display = 'none';
+    if (loanChartInstance) {
+        loanChartInstance.destroy();
+        loanChartInstance = null;
+    }
+    if (loanPaymentChartInstance) {
+        loanPaymentChartInstance.destroy();
+        loanPaymentChartInstance = null;
+    }
+}
+
+function formatNumber(number) {
+    return number.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+}
+
+function calculateLoanPayment(loanAmount, interestRate, loanTerm) {
+    var monthlyInterestRate = interestRate / 12;
+    return (loanAmount * monthlyInterestRate) / (1 - Math.pow(1 + monthlyInterestRate, -loanTerm));
+}
+
+function drawPieChart(principal, interest) {
+    var ctx = document.getElementById('loanChart').getContext('2d');
+    if (loanChartInstance) {
+        loanChartInstance.data.datasets[0].data = [principal, interest];
+        loanChartInstance.update();
+    } else {
+        loanChartInstance = new Chart(ctx, {
+            type: 'pie',
+            data: {
+                labels: ['Principal', 'Interest'],
+                datasets: [{
+                    data: [principal, interest],
+                    backgroundColor: ['#4e73df', '#1cc88a'],
+                    hoverBackgroundColor: ['#2e59d9', '#17a673'],
+                    hoverBorderColor: "rgba(234, 236, 244, 1)",
+                }],
+            },
+            options: {
+                maintainAspectRatio: false,
+                responsive: true,
+                width: 1,
+                height: 1, // Set the desired width and height
+                plugins: {
+                    title: {
+                        display: true,
+                        text: 'Loan Payment Breakdown (Principal vs. Interest)',
+                        font: {
+                            size: window.innerWidth < 800 ? 8 : 16 // Adjust title font size based on screen width
+                        }
+                    },
+                    // Adjust font size of labels based on screen width
+                    legend: {
+                        labels: {
+                            font: {
+                                size: window.innerWidth < 800 ? 8 : 12 // Adjust font size based on screen width
+                            }
+                        }
+                    }
+                },
+                tooltips: {
+                    backgroundColor: "rgb(255,255,255)",
+                    bodyFontColor: "#858796",
+                    borderColor: '#dddfeb',
+                    borderWidth: 1,
+                    xPadding: 15,
+                    yPadding: 15,
+                    displayColors: false,
+                    caretPadding: 10,
+                },
+                legend: {
+                    display: false
+                },
+                cutoutPercentage: 80,
+            },
+        });
+
+        // Add chart-labels class to chart labels
+        document.querySelectorAll('#loanChart .chartjs-text').forEach(label => {
+            label.classList.add('chart-labels');
+        });
+    }
+}
+
+
+
+
+
+function drawLoanPaymentChart(profit, payment) {
+    var ctx = document.getElementById('loanPaymentChart').getContext('2d');
+    if (loanPaymentChartInstance) {
+        loanPaymentChartInstance.data.datasets[0].data = [profit, payment];
+        loanPaymentChartInstance.update();
+    } else {
+        loanPaymentChartInstance = new Chart(ctx, {
+            type: 'pie',
+            data: {
+                labels: ['Profit After Loan Payments', 'Loan Payment'],
+                datasets: [{
+                    data: [profit, payment],
+                    backgroundColor: ['#1cc88a', '#f6c23e'],
+                    hoverBackgroundColor: ['#17a673', '#f4b619'],
+                    hoverBorderColor: "rgba(234, 236, 244, 1)",
+                }],
+            },
+            options: {
+                maintainAspectRatio: false,
+                responsive: true,
+                width: 1,
+                height: 1, // Set the desired width and height
+                plugins: {
+                    title: {
+                        display: true,
+                        text: 'Monthly Loan Payments as a Share of Monthly Income',
+                        font: {
+                            size: window.innerWidth < 800 ? 8 : 16 // Adjust title font size based on screen width
+                        }
+                    },
+                    legend: {
+                        labels: {
+                            font: {
+                                size: window.innerWidth < 800 ? 8 : 12 // Adjust font size based on screen width
+                            }
+                        }
+                    }
+                },
+                tooltips: {
+                    backgroundColor: "rgb(255,255,255)",
+                    bodyFontColor: "#858796",
+                    borderColor: '#dddfeb',
+                    borderWidth: 1,
+                    xPadding: 15,
+                    yPadding: 15,
+                    displayColors: true,
+                    caretPadding: 10,
+                },
+                cutoutPercentage: 50,
+            },
+        });
+
+        // Add chart-labels class to chart labels
+        document.querySelectorAll('#loanChart .chartjs-text').forEach(label => {
+            label.classList.add('chart-labels');
+        });
+    }
+}
+
+
+
+
+
+
+
+// Function to send queries to ChatGPT or a similar AI and other event listeners...
+
+// Function to send queries to ChatGPT or a similar AI
+async function askAI() {
+    const userQuery = document.getElementById('userQuery').value;
+    let augmentedQuery = userQuery;
+
+    // Check if calculator results are displayed
+    const resultElement = document.getElementById('result');
+    if (resultElement && resultElement.style.display !== 'none') {
+        const calculatorResults = resultElement.innerText;
+
+        augmentedQuery = `
+        Soon I will ask you a question. I want you to respond to my question, however if my question involves
+        something about this data, respond to my question given that data:
+
+        " ${calculatorResults} "
+
+        - However, if my question does not involve business or this data somehow, I want you to ignore the previous business
+        data and just answer my question normally.
+
+        - Ok, here is my question:
+
+        " ${userQuery} "
+
+        - also, moving forward, do not talk about the fact that you are doing this, or reference any of the data about the business unless specifically
+        asked to talk about it.
+
+        Also, I want you to take into account that you are a tool to help a potential investor that is interested in
+        buying a business. The investor is most importantly interested in generating a profit for himself and will be
+        especially focusing on the Return on investment he will get from his personal investment costs
+
+        Also a business is a better business if it generates more monthly cash flow minus the loan payment costs each month, the higher the ratio of
+        monthly cash flow to loan payment costs, the better the business is (make sure to mention this in your answer if applicable)
+        `;
+
+        console.log(augmentedQuery); // Log the augmentedQuery directly
+    }
+
+    const responseElement = document.getElementById('aiResponse');
+
+    try {
+        const response = await fetch('/ask-openai', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ prompt: augmentedQuery, max_tokens: 150 }) // Use the augmentedQuery here
+        });
+
+        const text = await response.text();
+
+        if (text) {
+            responseElement.innerText = text;
+            responseElement.style.display = 'block'; // Make the element visible
+        } else {
+            responseElement.innerText = 'No response from AI.';
+            responseElement.style.display = 'block'; // Make the element visible
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        responseElement.innerText = 'Failed to get response.';
+        responseElement.style.display = 'block'; // Make the element visible even in case of error
+    }
+}
+
+// Add event listener for 'Enter' keypress
+document.addEventListener('keypress', function(e) {
+    if (e.key === 'Enter') {
+        calculate();
+    }
+});
+
+// Ensure numeric inputs are correctly formatted
+document.querySelectorAll('input.numeric-input').forEach(input => {
+    input.addEventListener('input', function(e) {
+        this.value = this.value.replace(/[^0-9\.]/g, '');
+    });
+});
+
+document.getElementById('userQuery').addEventListener('keypress', function(e) {
+    if (e.key === 'Enter') {
+        // Check if text is entered in the input box
+        if (this.value.trim() !== '') {
+            askAI();
+        }
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const toggleBtn = document.getElementById('modeToggle');
+    const body = document.body;
+
+    toggleBtn.addEventListener('change', function () {
+        if (toggleBtn.checked) {
+            body.classList.add('dark-mode');
+        } else {
+            body.classList.remove('dark-mode');
+        }
+    });
+});
+
+function toggleChatbot() {
+    var chatbot = document.getElementById("chatbot");
+    chatbot.classList.toggle('active');
+    chatbot.style.display = (chatbot.style.display === "none") ? "block" : "none";
+}
+
---
+++ server.js
@@ -0,0 +1,69 @@
+// Allowing the use of .env files to add in API keys vs making them publically available and posting them to github
+import dotenv from 'dotenv';
+dotenv.config();
+
+// Importing modules using ESM syntax
+import express from 'express';
+import fetch from 'node-fetch';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+
+// added this apikey variable setup here
+const apiKey = process.env.OPENAI_API_KEY;
+
+const app = express();
+app.use(express.json());
+
+// Define __dirname in ESM
+const __dirname = dirname(fileURLToPath(import.meta.url));1
+
+// Serve static files
+app.use(express.static(join(__dirname, 'public')));
+
+
+// AI request handling endpoint
+app.post('/ask-openai', async (req, res) => {
+    try {
+        const response = await fetch('https://api.openai.com/v1/chat/completions', {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${apiKey}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                model: "gpt-3.5-turbo",
+                messages: [{ role: "user", content: req.body.prompt }]
+            })
+        });
+
+        //removing this for the test:
+        //const data = await response.json();
+        //res.send(data);
+        // end of what is removed
+        const data = await response.json();
+        console.log(data);  // Log the response data for debugging
+        
+        if (data.choices && data.choices.length > 0) {
+            res.send(data.choices[0].message.content);
+        } else {
+            res.status(500).send('Unexpected response structure from OpenAI API');
+        }
+        //end of what is being added
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send('An error occurred on the server.');
+    }
+});
+
+
+
+// Start the server
+// const PORT = 3000;
+// app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port http://localhost:${PORT}`);
+});
